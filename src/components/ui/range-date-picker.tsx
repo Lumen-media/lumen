da978e02ebc95fbbc25b5b7b03bf276d22516019
gabrielSantos1101 <gabriel.adv45@outlook.com>
2025-08-18 23:03:35 -0300

    Chore: Refine configuration files

--- src/components/ui/range-date-picker.tsx
+++ src/components/ui/range-date-picker.tsx
@@ -22,7 +22,6 @@ export function RangeDatePicker({
 	className,
 	date,
 	onDateChange,
-	...props
 }: RangeDatePickerProps) {
 	const [selectedDate, setSelectedDate] = React.useState<DateRange | undefined>(
 		date,
@@ -38,43 +37,42 @@ export function RangeDatePicker({
 	};
 
 	return (
-		<div className={cn("grid gap-2", className)} {...props}>
-			<Popover>
-				<PopoverTrigger asChild>
-					<Button
-						id="date"
-						variant={"outline"}
-						className={cn(
-							"w-[300px] justify-start text-left font-normal",
-							!selectedDate && "text-muted-foreground",
-						)}
-					>
-						<CalendarIcon className="mr-2 h-4 w-4" />
-						{selectedDate?.from ? (
-							selectedDate.to ? (
-								<>
-									{format(selectedDate.from, "LLL dd, y")} -{" "}
-									{format(selectedDate.to, "LLL dd, y")}
-								</>
-							) : (
-								format(selectedDate.from, "LLL dd, y")
-							)
+		<Popover>
+			<PopoverTrigger asChild>
+				<Button
+					id="date"
+					variant={"outline"}
+					className={cn(
+						"w-full justify-start text-left font-normal",
+						!selectedDate && "text-muted-foreground",
+						className,
+					)}
+				>
+					<CalendarIcon className="mr-2 h-4 w-4" />
+					{selectedDate?.from ? (
+						selectedDate.to ? (
+							<>
+								{format(selectedDate.from, "LLL dd, y")} -{" "}
+								{format(selectedDate.to, "LLL dd, y")}
+							</>
 						) : (
-							<span>Pick a date range</span>
-						)}
-					</Button>
-				</PopoverTrigger>
-				<PopoverContent className="w-auto p-0" align="start">
-					<Calendar
-						initialFocus
-						mode="range"
-						defaultMonth={selectedDate?.from}
-						selected={selectedDate}
-						onSelect={handleDateSelect}
-						numberOfMonths={2}
-					/>
-				</PopoverContent>
-			</Popover>
-		</div>
+							format(selectedDate.from, "LLL dd, y")
+						)
+					) : (
+						<span>Pick a date range</span>
+					)}
+				</Button>
+			</PopoverTrigger>
+			<PopoverContent className="w-auto p-0" align="start">
+				<Calendar
+					initialFocus
+					mode="range"
+					defaultMonth={selectedDate?.from}
+					selected={selectedDate}
+					onSelect={handleDateSelect}
+					numberOfMonths={2}
+				/>
+			</PopoverContent>
+		</Popover>
 	);
 }
