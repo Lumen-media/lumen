b27368491e595e397a97267be8d228760114a509
gabrielSantos1101 <gabriel.adv45@outlook.com>
2025-08-31 21:32:11 -0300

    feat: refactor RightPanel component to use dynamic tabs and improve styles

--- src/components/right-panel/index.tsx
+++ src/components/right-panel/index.tsx
@@ -1,3 +1,5 @@
+import type React from "react";
+import { useState } from "react";
 import { Card } from "../ui/card";
 import { Tabs, TabsList, TabsTrigger } from "../ui/tabs";
 import { LyricsTab } from "./lyrics-tab";
@@ -5,33 +7,39 @@ import { MediaTab } from "./media-tab";
 import { ThemesTab } from "./themes-tab";
 import "./styles.css";
 
+const tabs = ["media", "lyrics", "themes"];
+
 export const RightPanel = () => {
+	const [activeTab, setActiveTab] = useState("media");
+
 	return (
 		<Card className="w-full h-full">
-			<Tabs defaultValue="media" className="tabs">
+			<Tabs
+				defaultValue="media"
+				className="tabs"
+				onValueChange={(value) => setActiveTab(value)}
+			>
 				<div className="border-b">
 					<TabsList className="flex justify-between w-full gap-3 p-0 h-fit relative">
-						<TabsTrigger
-							value="media"
-							className="tabs__tab after:-bottom-[1px]"
-						>
-							Media
-						</TabsTrigger>
-						<TabsTrigger
-							value="lyrics"
-							className="tabs__tab after:-bottom-[1px]"
-						>
-							Lyrics
-						</TabsTrigger>
-						<TabsTrigger
-							value="themes"
-							className="tabs__tab after:-bottom-[1px]"
-						>
-							Themes
-						</TabsTrigger>
+						{tabs.map((tab) => (
+							<TabsTrigger
+								key={tab}
+								value={tab}
+								className="tab after:-bottom-[1px]"
+							>
+								{tab}
+							</TabsTrigger>
+						))}
+
 						<div
-							className="tabs__indicator h-0.5 bg-primary rounded-full translate-y-[1px] transition-all duration-200 ease-in-out"
+							className="indicator absolute w-full h-0.5 bg-primary rounded-full bottom-0 left-0 translate-y-[1px] transition-all duration-300 ease-in-out"
 							role="presentation"
+							style={
+								{
+									"--scaleX": 1 / tabs.length,
+									"--translateX": `${tabs.indexOf(activeTab) * (100 / tabs.length)}%`,
+								} as React.CSSProperties
+							}
 						/>
 					</TabsList>
 				</div>
