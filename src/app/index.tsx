c39c1b08eda59dcd08d8c141834163bcfd3cfd99
gabrielSantos1101 <gabriel.adv45@outlook.com>
2025-11-06 21:30:51 -0300

    feat: Configure initial and minimum sizes for layout panels

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -10,23 +10,25 @@ export const Route = createFileRoute("/")({
 });
 
 function RouteComponent() {
+	const viewportWidth = window.innerWidth;
+
 	return (
 		<main className="h-dvh p-2.5 flex flex-col gap-3">
 			<Header />
 			<ResizablePanelGroup direction="horizontal">
-				<ResizablePanel className="min-w-1/6">
+				<ResizablePanel defaultSize={16} minSize={16} className="">
 					<MediaPanel />
 				</ResizablePanel>
 
 				<ResizableHandle className="bg-transparent mx-1.5 w-0" />
 
-				<ResizablePanel className="w-2/3 min-w-1/2">
+				<ResizablePanel defaultSize={70} minSize={50}>
 					<MiddlePanel />
 				</ResizablePanel>
 
 				<ResizableHandle className="bg-transparent mx-1.5 w-0" />
 
-				<ResizablePanel className="min-w-1/6">
+				<ResizablePanel defaultSize={20} minSize={16}>
 					<RightPanel />
 				</ResizablePanel>
 			</ResizablePanelGroup>
